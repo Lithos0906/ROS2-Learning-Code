6434aece8b7e26ec92857ebe190f358d151abfad
Lithos0906 <lithos0515@gmail.com>
2025-10-31 14:58:27 +0800

    COMMIT FACE DETECT SERVICE

--- Chapter4/chapter4_ws/src/demo_python_service/setup.py
+++ Chapter4/chapter4_ws/src/demo_python_service/setup.py
@@ -25,7 +25,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'learn_face_detect=demo_python_service.learn_face_detect:main'
+            'learn_face_detect=demo_python_service.learn_face_detect:main',
+            'face_detect_node=demo_python_service.face_detect_node:main'
         ],
     },
 )
