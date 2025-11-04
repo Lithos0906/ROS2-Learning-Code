e9df2eef4fd94b309bb08226f1bb4eaf83107a64
Lithos0906 <lithos0515@gmail.com>
2025-11-05 01:27:14 +0800

    COMMIT PARAMS UPDATE SUB

--- Chapter4/chapter4_ws/src/demo_python_service/demo_python_service/face_detect_node.py
+++ Chapter4/chapter4_ws/src/demo_python_service/demo_python_service/face_detect_node.py
@@ -7,6 +7,7 @@ from ament_index_python.packages import get_package_share_directory
 import os
 from cv_bridge import CvBridge
 import time
+from rcl_interfaces.msg import SetParametersResult
 
 class FaceDetectNode(Node):
     def __init__(self):
@@ -19,6 +20,16 @@ class FaceDetectNode(Node):
         self.model = self.get_parameter('model').value
         self.default_image_path = os.path.join(get_package_share_directory('demo_python_service'), 'resource/default.jpg')
         self.get_logger().info(f"face detect service has been launched.")
+        self.add_on_set_parameters_callback(self.parameters_callback)
+
+    def parameters_callback(self, parameters):
+        for parameter in parameters:
+            self.get_logger().info(f"{parameter.name}->{parameter.value}")
+            if parameter.name == 'number_of_times_to_unsample':
+                self.number_of_times_to_upsample = parameter.value
+            if parameter.name == 'model':
+                self.model = parameter.value
+        return SetParametersResult(successful=True)
 
     def detect_face_callback(self, request, response):
         if request.image.data:
