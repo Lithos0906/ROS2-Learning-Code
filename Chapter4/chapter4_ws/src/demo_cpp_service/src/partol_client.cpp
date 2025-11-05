3ffd2168756ce420f7e655b81e6436ac32923f2a
Lithos0906 <lithos0515@gmail.com>
2025-11-05 23:25:33 +0800

    COMMIT PARAMS UPDATE IN CLIENT

--- Chapter4/chapter4_ws/src/demo_cpp_service/src/partol_client.cpp
+++ Chapter4/chapter4_ws/src/demo_cpp_service/src/partol_client.cpp
@@ -2,8 +2,13 @@
 #include "chapter4_interface/srv/partol.hpp"
 #include <chrono>
 #include <ctime>
+#include "rcl_interfaces/msg/parameter.hpp"
+#include "rcl_interfaces/msg/parameter_value.hpp"
+#include "rcl_interfaces/msg/parameter_type.hpp"
+#include "rcl_interfaces/srv/set_parameters.hpp"
 
 using Partol = chapter4_interface::srv::Partol;
+using SetP = rcl_interfaces::srv::SetParameters;
 using namespace std::chrono_literals;
 
 class PartolClient: public rclcpp::Node
@@ -49,12 +54,71 @@ public:
             });
         });
     }
+
+    // create a client and send request, returning results
+    SetP::Response::SharedPtr call_set_parameters(const rcl_interfaces::msg::Parameter &param)
+    {
+        auto param_client = this->create_client<SetP>("/turtle_control/set_parameters");
+        // 1.determine if the server is online
+        while(!param_client->wait_for_service(1s))
+        {
+            if(!rclcpp::ok())
+            {
+                RCLCPP_ERROR(this->get_logger(), "While waiting for the service to be launched, rclcpp crashed, so I logged out");
+                return nullptr;
+            }
+            RCLCPP_INFO(this->get_logger(), "While waiting for the service to be launched.....");
+        }
+
+        // 2.structure Request
+        auto request = std::make_shared<SetP::Request>();
+        request->parameters.push_back(param);
+
+        // 3. send a request and wait for processing to complete
+        auto future = param_client->async_send_request(request);
+        rclcpp::spin_until_future_complete(this->get_node_base_interface(), future);
+        auto response = future.get();
+        return response;
+    }
+
+    // update param K
+    void update_server_param_k(double k)
+    {
+        // 1.create a param orient
+        auto param = rcl_interfaces::msg::Parameter();
+        param.name = "k";
+
+        // 2.create value
+        auto param_value = rcl_interfaces::msg::ParameterValue();
+        param_value.type = rcl_interfaces::msg::ParameterType::PARAMETER_DOUBLE;
+        param_value.double_value = k;
+        param.value = param_value;
+
+        // 3.request to update params and handle
+        auto response = this->call_set_parameters(param);
+        if(response == NULL)
+        {
+            RCLCPP_INFO(this->get_logger(), "params update failed");
+        }
+        for(auto result:response->results)
+        {
+            if(result.successful == false)
+            {
+                RCLCPP_INFO(this->get_logger(), "params update failed, reason: %s", result.reason.c_str());
+            }
+            else
+            {
+                RCLCPP_INFO(this->get_logger(), "params update successfully");
+            }
+        }
+    }
 };
 
 int main(int argc, char* argv[])
 {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<PartolClient>();
+    node->update_server_param_k(4.0);
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
