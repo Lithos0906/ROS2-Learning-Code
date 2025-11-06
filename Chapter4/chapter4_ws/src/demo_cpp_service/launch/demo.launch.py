ab6baccabea1a859785c8014263c085ffc2f54a2
Lithos0906 <lithos0515@gmail.com>
2025-11-06 19:22:34 +0800

    COMMIT LAUNCH PASS PARAMS

--- Chapter4/chapter4_ws/src/demo_cpp_service/launch/demo.launch.py
+++ Chapter4/chapter4_ws/src/demo_cpp_service/launch/demo.launch.py
@@ -2,10 +2,16 @@ import launch
 import launch_ros
 
 def generate_launch_description():
+    # 1.declare a launch param
+    action_declare_arg_background_g = launch.actions.DeclareLaunchArgument('launch_arg_bg', default_value="150")
+
+    # 2.manually pass the launch parameter to a node
     # generate the description of launch
     action_node_turtlesim_node = launch_ros.actions.Node(
         package = 'turtlesim',
         executable = 'turtlesim_node',
+        # obtain the launch value
+        parameters=[{'background_g': launch.substitutions.LaunchConfiguration('launch_arg_bg', default="150")}],
         output = 'screen'
     )
 
@@ -23,6 +29,7 @@ def generate_launch_description():
 
     return launch.LaunchDescription([
         # actions
+        action_declare_arg_background_g,
         action_node_turtlesim_node,
         action_node_turtle_control,
         action_node_partol_client
