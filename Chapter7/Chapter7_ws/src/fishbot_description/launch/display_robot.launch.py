6c570bc6361961f083f223bc8bd4a2259e337760
Lithos0906 <lithos0515@gmail.com>
2026-03-22 16:24:18 +0800

    commit chapter7

---
+++ Chapter7/Chapter7_ws/src/fishbot_description/launch/display_robot.launch.py
@@ -0,0 +1,43 @@
+import launch
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    # obtain the default urdf path
+    urdf_package_path = get_package_share_directory('fishbot_description')
+    default_urdf_path = os.path.join(urdf_package_path, 'urdf', 'first_robot.urdf')
+    default_rviz_configure_path = os.path.join(urdf_package_path, 'configure', 'display_robot_model.rviz')
+    # declare a content param of urdf
+    action_declare_arg_mode_path = launch.actions.DeclareLaunchArgument(
+        name='model', default_value=str(default_urdf_path), description="path of loading model file"
+    )
+    # the content is obtained from the file path
+    # and converted into a parameter value object
+    # to be passed to robot_state_publisher
+    substitutions_command_result = launch.substitutions.Command(['xacro ', launch.substitutions.LaunchConfiguration('model')])
+    robot_description_value = launch_ros.parameter_descriptions.ParameterValue(substitutions_command_result, value_type=str)
+
+    action_robot_state_publisher = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description':robot_description_value}]
+    )
+
+    action_joint_state_publisher = launch_ros.actions.Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+    )
+
+    action_rviz_node = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', default_rviz_configure_path]
+    )
+
+    return launch.LaunchDescription([
+        action_declare_arg_mode_path,
+        action_robot_state_publisher,
+        action_joint_state_publisher,
+        action_rviz_node,
+    ])
